6906ab26d80ebc9bf1e7e38676669e0e0d31458b
Vit V <tesla.v3@gmail.com>
2019-10-20 23:40:16 +0600

    init

---
+++ main.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	//"strings"
+	"time"
+)
+
+var bigDigit = map[string][]string{
+	"0": {" 00 ",
+		"0  0",
+		"0  0",
+		"0  0",
+		" 00 "},
+	"1": {"  1 ",
+		" 11 ",
+		"  1 ",
+		"  1 ",
+		" 111"},
+	"2": {" 22 ",
+		"2  2",
+		"  2 ",
+		"2   ",
+		"2222"},
+	"3": {" 33 ",
+		"3  3",
+		"  3 ",
+		"3  3",
+		" 33 "},
+	"4": {"4  4",
+		"4  4",
+		"4444",
+		"   4",
+		"   4"},
+	"5": {"5555",
+		"5   ",
+		"555 ",
+		"   5",
+		"555 "},
+	"6": {" 66 ",
+		"6   ",
+		"666 ",
+		"6  6",
+		" 66 "},
+	"7": {"7777",
+		"   7",
+		"  7 ",
+		" 7  ",
+		"7   "},
+	"8": {" 88 ",
+		"8  8",
+		" 88 ",
+		"8  8",
+		" 88 "},
+	"9": {" 99 ",
+		"9  9",
+		" 999",
+		"   9",
+		" 99 "},
+	"-": {"    ",
+		"    ",
+		"----",
+		"    ",
+		"    "},
+}
+
+func TimeTrack(start time.Time) {
+	timeRun := time.Since(start)
+	fmt.Print("run time: ", timeRun, "\n")
+
+}
+
+func main() {
+	if len(os.Args) == 1 {
+		os.Exit(1)
+	}
+	defer TimeTrack(time.Now())
+	// fmt.Print(os.Args[1])
+	var result [5]string
+	repetition := 10000
+	for i := 0; i < repetition; i++ {
+		//result = [5]string{"", "", "", "", ""}
+		for _, dig := range os.Args[1] {
+			// fmt.Print(string(dig) + "\n")
+			if key, ok := bigDigit[string(dig)]; ok {
+				// fmt.Print(key[0] + " \n")
+				//for index, _ := range result {
+				//	result[index] += key[index]
+				//}
+				result[0] += key[0]
+				result[1] += key[1]
+				result[2] += key[2]
+				result[3] += key[3]
+				result[4] += key[4]
+			}
+		}
+	}
+
+	//fmt.Println(strings.Join(result[:], "\n"))
+
+	//for _, line :=range result{
+	//	fmt.Println(len(line))
+	//}
+}
